08305c04bb3fb2cc4c0eb8f77e2403c866728f1a
hitode909 <hitode909@gmail.com>
2017-12-11 18:56:33 +0900

    speech

---
+++ speech.js
@@ -0,0 +1,32 @@
+/*
+
+* はてなブログに「音読する」ボタンを追加する
+
+** 使い方
+
+- 以下をコピーして デザイン編集 → カスタマイズ → フッタHTML に貼り付け
+
+<script async defer src="http://hitode909.github.io/hatenablog-unofficial-modules/speech.js"></script>
+
+** 注意
+- Web Speech API非対応のブラウザでは音読できません
+
+*/
+
+(function () {
+  if (!window.speechSynthesis) return;
+  document.querySelectorAll('article').forEach(function (article) {
+    var button = document.createElement('button');
+    button.type = 'button';
+    button.className = 'btn';
+    button.textContent = '音読する';
+    button.style = 'float: right';
+    article.querySelector('header').appendChild(button);
+    button.addEventListener('click', function () {
+      var synth = window.speechSynthesis;
+      var body = article.querySelector('.entry-content').textContent;
+      var utter = new SpeechSynthesisUtterance(body);
+      synth.speak(utter);
+    });
+  });
+})();
